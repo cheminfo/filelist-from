84fa5c5a0ae9cfd40d3b7f925308bb44b0f74036
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-03-16 16:22:17 +0100

    feat: add checkZip option to fileListUnzip

--- src/__tests__/fileListUnzip.test.ts
+++ src/__tests__/fileListUnzip.test.ts
@@ -6,7 +6,9 @@ import { fileListUnzip } from '../fileListUnzip';
 describe('folder with zip files', () => {
   it('unzip fileList', async () => {
     const normalFileList = fileListFromPath(join(__dirname, 'dataUnzip'));
-    const fileListUnzipped = await fileListUnzip(normalFileList);
+    const fileListUnzipped = await fileListUnzip(normalFileList, {
+      checkZip: true,
+    });
 
     expect(
       Array.from(
@@ -30,8 +32,8 @@ describe('folder with zip files', () => {
       '/data.zip/data/dir1/b.txt - b.txt',
       '/data.zip/data/dir1/dir3/e.txt - e.txt',
       '/data.zip/data/dir1/dir3/f.txt - f.txt',
-      '/dir2/data.zip/data/subDir1/c.txt - c.txt',
-      '/dir2/data.zip/data/subDir1/d.txt - d.txt',
+      '/dir2/data.zipped/data/subDir1/c.txt - c.txt',
+      '/dir2/data.zipped/data/subDir1/d.txt - d.txt',
       '/data.zip/data/dir1/dir3/zipFile3.zip/c.txt - c.txt',
       '/data.zip/data/dir1/dir3/zipFile3.zip/d.txt - d.txt',
     ]);
--- src/fileListUnzip.ts
+++ src/fileListUnzip.ts
@@ -8,10 +8,19 @@ interface FileList {
   text: () => Promise<string>;
   arrayBuffer: () => Promise<ArrayBuffer>;
 }
-export async function fileListUnzip(fileList: FileList[]) {
+
+const FILES_SIGNATURES = {
+  ZIP: '504b0304',
+};
+export async function fileListUnzip(
+  fileList: FileList[],
+  options: { checkZip?: boolean } = {},
+) {
+  const { checkZip = false } = options;
   for (let i = 0; i < fileList.length; i++) {
-    if (!/\.zip$/.exec(fileList[i].name)) continue;
-    const zipBuffer = await fileList[i].arrayBuffer();
+    let { isZipped, buffer } = await checkIfZip(fileList[i], checkZip);
+    if (!isZipped) continue;
+    const zipBuffer = !buffer ? await fileList[i].arrayBuffer() : buffer;
     const currentFileList = await fileListFromZip(zipBuffer);
     for (let subFile of currentFileList) {
       subFile.webkitRelativePath = `${fileList[i].webkitRelativePath}/${subFile.webkitRelativePath}`;
@@ -21,4 +30,19 @@ export async function fileListUnzip(fileList: FileList[]) {
   }
 
   return fileList;
+}
+
+async function checkIfZip(file: FileList, checkZip: boolean) {
+  if (checkZip) {
+    const buffer = await file.arrayBuffer();
+    const signature = getFileSignature(buffer);
+    return { isZipped: signature === FILES_SIGNATURES.ZIP, buffer };
+  }
+  return { isZipped: /\.zip$/.exec(file.name) };
+}
+
+function getFileSignature(fileArrayBuffer: ArrayBuffer) {
+  return new Uint8Array(fileArrayBuffer)
+    .slice(0, 4)
+    .reduce((acc, byte) => (acc += byte.toString(16).padStart(2, '0')), '');
 }
