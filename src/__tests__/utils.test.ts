dc5e5a60fe390e73c620baa3e003ba2b6589a1e8
sm <santimir@protonmail.com>
2022-03-29 18:13:02 +0100

    fix: a few typos and eslint rules

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -4,41 +4,41 @@ import { fileListFromPath } from '../fileListFromPath';
 import { groupFiles } from '../utils';
 
 describe('file grouping', () => {
-    it('Default options', () => {
-        const files = fileListFromPath(join(__dirname, 'data/dir3'));
-        const result = groupFiles(files, {
-            idWithBasename: true,
-            fileKey:'extension',
-            applyToFileKey: (fk) => fk.toLowerCase()
-        });
-        expect(result).toHaveLength(1);
-        expect(Object.keys(result[0])).toEqual(["gid", "mpt", "mpr","mps"])
+  it('Default options', () => {
+    const files = fileListFromPath(join(__dirname, 'data/dir3'));
+    const result = groupFiles(files, {
+      idWithBasename: true,
+      fileKey: 'extension',
+      applyToFileKey: (fk) => fk.toLowerCase(),
     });
+    expect(result).toHaveLength(1);
+    expect(Object.keys(result[0])).toStrictEqual(['gid', 'mpt', 'mpr', 'mps']);
+  });
 
-    it('Subset of options', () => {
-        const files = fileListFromPath(join(__dirname, 'data/dir1'));
-        const result = groupFiles(files, {
-            idWithBasename: false,
-            fileKey: 'basename',
-            applyToFileKey: (fk) => fk.toUpperCase()
-        });
-      expect(result).toHaveLength(2)
-      expect(result[1]["F"]).toMatchObject({
-          name: 'f.txt',
-          size: 1,
-          lastModified: 1648568347191,
-        })
+  it('Subset of options', () => {
+    const files = fileListFromPath(join(__dirname, 'data/dir1'));
+    const result = groupFiles(files, {
+      idWithBasename: false,
+      fileKey: 'basename',
+      applyToFileKey: (fk) => fk.toUpperCase(),
     });
+    expect(result).toHaveLength(2);
+    expect(result[1].F).toMatchObject({
+      name: 'f.txt',
+      size: 1,
+      lastModified: 1648568347191,
+    });
+  });
 
-    it('Subset 2 of options', () => {
-        const files = fileListFromPath(join(__dirname, 'data'));
-        const result = groupFiles(files, {
-            idWithBasename: false,
-            fileKey: 'filename',
-            captureFromPath: /\/(?<P2>\w+)\/(?<P1>\w+)$/,
-        });
-      const first = result[0]
-      expect(first.gid).toMatch(/.*\/data\/dir1/)
-      expect(first.captureGroups).toMatchObject({ P2: 'data', P1: 'dir1' })
+  it('Subset 2 of options', () => {
+    const files = fileListFromPath(join(__dirname, 'data'));
+    const result = groupFiles(files, {
+      idWithBasename: false,
+      fileKey: 'filename',
+      captureFromPath: /\/(?<P2>\w+)\/(?<P1>\w+)$/,
     });
+    const first = result[0];
+    expect(first.gid).toMatch(/.*\/data\/dir1/);
+    expect(first.captureGroups).toMatchObject({ P2: 'data', P1: 'dir1' });
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,14 @@
-import {PartialFile, PartialFileList} from "./PartialFile"
-export type StringObject = Record<string,string>;
+import { PartialFileList } from './PartialFile';
 
-/** 
- * Type for [[`groupFiles`]] function 
+export type StringObject = Record<string, string>;
+
+/**
+ * Type for [[`groupFiles`]] function
  */
-export type GroupFiles = (flist:PartialFileList | FileList, options: GroupFilesOptions) => GroupedFiles[]
+export type GroupFiles = (
+  flist: PartialFileList | FileList,
+  options: GroupFilesOptions,
+) => GroupedFiles[];
 /**
  * Group files under same directory path
  * i.e `[Dir1, Dir2, Dir3...]`  where each Dir is
@@ -24,57 +28,55 @@ export type GroupFiles = (flist:PartialFileList | FileList, options: GroupFilesO
  * @return - array of dirs. Some keys are File objects (`fileA`, `fileB`..).
  */
 
-export const groupFiles: GroupFiles = ( fileList, options =  { idWithBasename: true, fileKey: "extension" }) => {
-    const { 
-      idWithBasename, 
-      fileKey,
-      applyToFileKey,
-      captureFromPath
-     } = options;
+export const groupFiles: GroupFiles = (
+  fileList,
+  options = { idWithBasename: true, fileKey: 'extension' },
+) => {
+  const { idWithBasename, fileKey, applyToFileKey, captureFromPath } = options;
 
-    type ID = string;
-    let results: Record<ID,Partial<GroupedFiles>> = {};
+  type ID = string;
+  let results: Record<ID, Partial<GroupedFiles>> = {};
 
-    for (const file of fileList) {
-        /* name */
-        const dirPath = file.webkitRelativePath.replace(/\/[^/]+$/, '');
-        const filename = applyToFileKey ? applyToFileKey(file.name) : file.name ;
-        const basename = filename.replace(/\.[^.]+$/, '');
-        const extension = filename.replace(/.*\./, '');
+  for (const file of fileList) {
+    /* name */
+    const dirPath = file.webkitRelativePath.replace(/\/[^/]+$/, '');
+    const filename = applyToFileKey ? applyToFileKey(file.name) : file.name;
+    const basename = filename.replace(/\.[^.]+$/, '');
+    const extension = filename.replace(/.*\./, '');
 
-        /* Group ID */
-        let gid;
-        if (idWithBasename) {
-            gid = dirPath + "/" + basename
-        } else {
-            //just dir path, like for WDF parser.
-            gid = dirPath
-        }
+    /* Group ID */
+    let gid;
+    if (idWithBasename) {
+      gid = `${dirPath}/${basename}`;
+    } else {
+      //just dir path, like for WDF parser.
+      gid = dirPath;
+    }
 
-        /* set the unique grouping id */
-        if(!results[gid]) results[gid] = { gid };
+    /* set the unique grouping id */
+    if (!results[gid]) results[gid] = { gid };
 
-        /* captured information from regex */
-        if(captureFromPath && !results[gid]["captureGroups"]) {
-          const cGroups = dirPath.match(captureFromPath)?.groups
-          if(cGroups) results[gid]["captureGroups"] = cGroups
-        }
+    /* captured information from regex */
+    if (captureFromPath && !results[gid].captureGroups) {
+      const cGroups = dirPath.match(captureFromPath)?.groups;
+      if (cGroups) results[gid].captureGroups = cGroups;
+    }
 
-        /* place file under a particular group `results[gid]` */
-        switch(fileKey){
-          case 'extension':
-            results[gid][extension] =  file;
-            break
-          case 'basename':
-            results[gid][basename] = file;
-            break
-          default:
-            results[gid][filename] = file;
-            break
-        }
-}
-    return Object.keys(results).map((gid) => results[gid]) as GroupedFiles[];
-}
+    /* place file under a particular group `results[gid]` */
+    switch (fileKey) {
+      case 'extension':
+        results[gid][extension] = file;
+        break;
+      case 'basename':
+        results[gid][basename] = file;
+        break;
+      default:
+        results[gid][filename] = file;
+        break;
+    }
+  }
+  return Object.keys(results).map((gid) => results[gid]) as GroupedFiles[];
+};
 
 /**
  * Interface representing Grouped files as done by [[`groupFiles`]]
@@ -82,17 +84,17 @@ export const groupFiles: GroupFiles = ( fileList, options =  { idWithBasename: t
  * groupFiles will return an array of these.
  */
 export interface GroupedFiles {
-    /** group id used to group the files under same path */
-    gid: string;
-    /** file name as taken from [[`File`]] or compatible class.*/
-    name: string;
-    /** file extension if it has an extension */
-    extension?: string;
-    /** Object with groups captured by RegEx. See [[`GroupFilesOptions`]]. */
-    captureGroups:StringObject;
-    /** there is no other way atm, this will store the files */
-    // eslint-disable-next-line
-    [file: string]: any;
+  /** group id used to group the files under same path */
+  gid: string;
+  /** file name as taken from [[`File`]] or compatible class.*/
+  name: string;
+  /** file extension if it has an extension */
+  extension?: string;
+  /** Object with groups captured by RegEx. See [[`GroupFilesOptions`]]. */
+  captureGroups: StringObject;
+  /** there is no other way atm, this will store the files */
+  // eslint-disable-next-line
+  [file: string]: any;
 }
 
 type NewFilename = string;
@@ -100,25 +102,25 @@ type NewFilename = string;
  * Options for [[`groupFiles`]]
  */
 export interface GroupFilesOptions {
-    /** A path like `./dir1/dir2/base.ext` will use `./dir1/dir2/base` as id.
-     * Sets `extensionAsKey:true`. Can segregate files. */
-    idWithBasename?: boolean;
-    /** 
-     * Example:
-     * Take the path `./dir1/dir2/base.ext`
-     * * With 'basename'  File be at `base` key 
-     * * With 'extension' File at `ext` key.
-     * * With 'filename' (default) File at `base.ext` key.
-     * If the fileKey is 'extension' but the file doesn't have one 
-     * (pretty common in a million files) it takes the basename.
-     * If you lowercase them and end up being the same, one will be overwritten 
-     * (we can't have 2 same-name keys in an object). 
-     */
-    fileKey?: 'basename'|'extension'|'filename'
-    /** For example, `(filename) => filename.toLowerCase()` 
-     * and the file `a.EXT` will be at key `ext` */
-    applyToFileKey?: (filename:string) => NewFilename,
-    /** Optional regex to name-capture groups from path.
+  /** A path like `./dir1/dir2/base.ext` will use `./dir1/dir2/base` as id.
+   * Sets `extensionAsKey:true`. Can segregate files. */
+  idWithBasename?: boolean;
+  /**
+   * Example:
+   * Take the path `./dir1/dir2/base.ext`
+   * * With 'basename'  File be at `base` key
+   * * With 'extension' File at `ext` key.
+   * * With 'filename' (default) File at `base.ext` key.
+   * If the fileKey is 'extension' but the file doesn't have one
+   * (pretty common in a million files) it takes the basename.
+   * If you lowercase them and end up being the same, one will be overwritten
+   * (we can't have 2 same-name keys in an object).
+   */
+  fileKey?: 'basename' | 'extension' | 'filename';
+  /** For example, `(filename) => filename.toLowerCase()`
+   * and the file `a.EXT` will be at key `ext` */
+  applyToFileKey?: (filename: string) => NewFilename;
+  /** Optional regex to name-capture groups from path.
      * Could be useful for post processing and filtering.
      * Example:
      * ```
@@ -132,5 +134,5 @@ export interface GroupFilesOptions {
         }
        ```
      */
-    captureFromPath?:RegExp
+  captureFromPath?: RegExp;
 }
