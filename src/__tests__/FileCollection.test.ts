d6a1b4ad1c65cb966348b05eeb5f43de6f5df4dd
Santi Miranda <santimir@protonmail.com>
2022-11-07 14:02:58 +0100

    feat(find): add method to class

--- src/__tests__/FileCollection.test.ts
+++ src/__tests__/FileCollection.test.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/prefer-regexp-exec */
 import { join } from 'path';
 
+import type { FileCollectionItem } from '../FileCollectionItem';
 import { fileCollectionFromPath } from '../fileCollectionFromPath';
 
 describe('FileCollection', () => {
@@ -16,5 +16,23 @@ describe('FileCollection', () => {
     expect(
       Array.from(filtered.files.map((a) => `${a.relativePath} - ${a.name}`)),
     ).toStrictEqual(['dir1/dir3/e.txt - e.txt', 'dir1/dir3/f.txt - f.txt']);
+  });
+
+  it('find', async () => {
+    const fileCollection = await fileCollectionFromPath(
+      join(__dirname, '../__tests__/data/dir1'),
+    );
+
+    const found = fileCollection.find((file) =>
+      /e.txt/.test(file.name),
+    ) as FileCollectionItem;
+    const notFound = fileCollection.find((file) =>
+      /notFound.txt/.test(file.name),
+    ) as undefined;
+
+    expect(`${found.relativePath} - ${found.name}`).toStrictEqual(
+      'dir1/dir3/e.txt - e.txt',
+    );
+    expect(notFound).toBeUndefined();
   });
 });
