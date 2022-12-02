0916abec149b7394d41fd017bf0a06c45de1e09e
Luc Patiny <luc@patiny.com>
2022-12-02 07:29:47 +0100

    feat: add fileCollectionFromFileArray
    closes: https://github.com/cheminfo/filelist-utils/issues/60

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './fileCollectionFromPaths';
 export * from './fileCollectionFromZip';
 export * from './fileCollectionFromWebservice';
 export * from './fileCollectionFromFileList';
+export * from './fileCollectionFromFileArray';
 export * from './fileCollectionFromFiles';
 export * from './groupFiles';
 export * from './FileCollection';
