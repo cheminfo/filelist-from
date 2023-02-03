dd440e04f710d51040b97deda10097041f1a6113
Luc Patiny <luc@patiny.com>
2023-02-03 13:17:54 +0100

    addArrayBuffer

--- src/FileCollection.ts
+++ src/FileCollection.ts
@@ -22,7 +22,87 @@ export class FileCollection {
     return fileCollectionToZip(this);
   }
 
+  addText(
+    relativePath: string,
+    text: string,
+    options: { dateModified?: number } = {},
+  ) {
+    this.files.push(getItemFromText(relativePath, text, options));
+  }
+
+  addArrayBuffer(
+    relativePath: string,
+    arrayBuffer: ArrayBuffer,
+    options: { dateModified?: number } = {},
+  ) {
+    this.files.push(getItemFromArrayBuffer(relativePath, arrayBuffer, options));
+  }
+
+  addTypedArray(
+    relativePath: string,
+    typedArray: Uint8Array,
+    options: { dateModified?: number } = {},
+  ) {
+    this.files.push(getItemFromTypedArray(relativePath, typedArray, options));
+  }
+
   [Symbol.iterator]() {
     return this.files.values();
   }
+}
+
+function getItemFromText(
+  relativePath: string,
+  text: string,
+  options: { dateModified?: number } = {},
+): FileCollectionItem {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(text);
+  return {
+    relativePath,
+    name: relativePath.split('/').pop() as string,
+    lastModified: options.dateModified || Date.now(),
+    size: data.length,
+    text: async () => {
+      return text;
+    },
+    arrayBuffer: async () => {
+      return data.buffer;
+    },
+    stream: () => {
+      throw new Error('stream no implemented');
+    },
+  };
+}
+
+function getItemFromArrayBuffer(
+  relativePath: string,
+  arrayBuffer: ArrayBuffer,
+  options: { dateModified?: number } = {},
+): FileCollectionItem {
+  const decoder = new TextDecoder();
+  const text = decoder.decode(arrayBuffer);
+  return {
+    relativePath,
+    name: relativePath.split('/').pop() as string,
+    lastModified: options.dateModified || Date.now(),
+    size: arrayBuffer.byteLength,
+    text: async () => {
+      return text;
+    },
+    arrayBuffer: async () => {
+      return arrayBuffer;
+    },
+    stream: () => {
+      throw new Error('stream no implemented');
+    },
+  };
+}
+
+function getItemFromTypedArray(
+  relativePath: string,
+  typedArray: Uint8Array,
+  options: { dateModified?: number } = {},
+): FileCollectionItem {
+  return getItemFromArrayBuffer(relativePath, typedArray.buffer, options);
 }
--- src/__tests__/FileCollection.test.ts
+++ src/__tests__/FileCollection.test.ts
@@ -16,6 +16,12 @@ describe('FileCollection', () => {
     expect(
       Array.from(filtered.files.map((a) => `${a.relativePath} - ${a.name}`)),
     ).toStrictEqual(['dir1/dir3/e.txt - e.txt', 'dir1/dir3/f.txt - f.txt']);
+  });
+
+  it('zip', async () => {
+    const fileCollection = await fileCollectionFromPath(
+      join(__dirname, '../__tests__/data/dir1'),
+    );
 
     const zipped = await fileCollection.zip();
     expect(zipped).toHaveLength(612);
@@ -34,5 +40,43 @@ describe('FileCollection', () => {
       'dir1/dir3/e.txt - e.txt',
       'dir1/dir3/f.txt - f.txt',
     ]);
+  });
+
+  it('addText addArrayBuffer addTypedArray', async () => {
+    const fileCollection = await fileCollectionFromPath(
+      join(__dirname, '../__tests__/data/dir1'),
+    );
+
+    fileCollection.addText('text', 'Hello');
+
+    const uint8Array = Uint8Array.from([65, 66, 67, 68, 69]);
+
+    fileCollection.addTypedArray('typedArray', uint8Array);
+
+    fileCollection.addArrayBuffer('arrayBuffer', uint8Array.buffer);
+
+    const textEntry = fileCollection.files.filter(
+      (file) => file.name === 'text',
+    )[0];
+
+    const resultText = new Uint8Array(await textEntry.arrayBuffer());
+    expect(resultText).toHaveLength(5);
+    expect(Array.from(resultText)).toStrictEqual([72, 101, 108, 108, 111]);
+
+    const uint8ArrayEntry = fileCollection.files.filter(
+      (file) => file.name === 'typedArray',
+    )[0];
+
+    const resultUint8Array = await uint8ArrayEntry.text();
+    expect(resultUint8Array).toHaveLength(5);
+    expect(resultUint8Array).toBe('ABCDE');
+
+    const arrayBufferEntry = fileCollection.files.filter(
+      (file) => file.name === 'arrayBuffer',
+    )[0];
+
+    const resultArrayBuffer = await arrayBufferEntry.text();
+    expect(resultArrayBuffer).toHaveLength(5);
+    expect(resultArrayBuffer).toBe('ABCDE');
   });
 });
